ef4c34b938922bddf1888a4028653f5139014785
Edvinas <52853688+Edvinas27@users.noreply.github.com>
2025-06-28 19:11:58 +0300

    Added PasswordView starting design and PasswordView styles

--- ViewModels/PasswordsViewModel.cs
+++ ViewModels/PasswordsViewModel.cs
@@ -1,11 +1,32 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using PasswordManager.Data;
 
 namespace PasswordManager.ViewModels;
 
 public class PasswordsViewModel : PageViewModel
 {
+    public ObservableCollection<Password> Passwords { get; }
+    
     public PasswordsViewModel()
     {
         PageName = ApplicationPageNames.Passwords;
+
+        var passwords = new List<Password>
+        {
+            new Password("Email", "strongEmail123!"),
+            new Password("Bank", "secureBankPass456$"),
+            new Password("Social Media", "socialPass789#"),
+            new Password("Work", "workAccess321@"),
+            new Password("Gaming", "gameOn567%"),
+            new Password("Shopping", "shopSafe999^"),
+            new Password("Streaming", "streamflix432&")
+        };
+        
+        Passwords = new ObservableCollection<Password>(passwords);
     }
+}
+
+public record Password(string Name, string Value, bool IsFavourite = false)
+{
 }
